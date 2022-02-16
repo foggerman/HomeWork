933b553845a5b109284f7de361cc5267247213de
Foggerman <foggermanishe@protonmail.com>
2022-02-16 21:56:21 +0300

    Куча времени потрачено на поиск способа обрезать путь с конца до первого слеша...

--- z5/gettoo.sh
+++ z5/gettoo.sh
@@ -8,31 +8,32 @@ gARC=amd64
 gBILD=$(curl -s http://distfiles.gentoo.org/releases/$gARC/autobuilds/latest-install-$gARC-minimal.txt | grep -oE '^[^#\/]+')
 gISO="install-$gARC-minimal-$gBILD.iso"
 #gSTATE=admincd)
-#gMIR=http://ftp.vectranet.pl/gentoo/releases/amd64/autobuilds/current-install-amd64-minimal/
+#gMIRr=http://ftp.vectranet.pl/gentoo/releases/amd64/autobuilds/current-install-amd64-minimal/
 gURL="http://distfiles.gentoo.org/releases/$gARC/autobuilds/current-install-$gARC-minimal/$gISO"
 gDIR="$(pwd)"/ISO
 #DIGESTS.asc
 #-o "$gDIR" grep -Eo '\b.+asc$'
 
 update-key() {
-wget -O - https://qa-reports.gentoo.org/output/service-keys.gpg | gpg --import
+wget -O - https://qa-reports.gentoo.org/output/service-keys.gpg | gpg --import #impory new key
 #list=$(ls "$gDIR" | grep -Eo '\w+asc$')
 
 if ! [ -f "$gDIR/$gISO.asc" ] #if not exist
-then
-wget -P "$gDIR" "$gURL.asc"  
-    elif ! [ -f "$gDIR/$gISO.DIGESTS" ] #if not exist
-    then
-    wget -P "$gDIR" "$gURL.DIGESTS"
+  then
+    wget -P "$gDIR" "$gURL.asc"  #download sign
+elif ! [ -f "$gDIR/$gISO.DIGESTS" ] #if not exist
+  then
+    wget -P "$gDIR" "$gURL.DIGESTS" #download SHA512
 else
-echo "Latest version already exist in $gDIR"
+  echo "Latest version already exist in $gDIR"
 fi
 
-for file in $gDIR/*asc
-do
+# verefi all gpg key
+for file in "$gDIR"/*asc
+  do
     if ! gpg --verify "$file"
-    then
-    echo "ERROR! failed verify $file"
+      then
+        echo "ERROR! failed verify $file"
     fi
 done
 }
@@ -40,18 +41,23 @@ done
 resdownload() {
 
 if ! [ -f "$gDIR/$gISO" ] #if not exist iso
-then
-wget -P "$gDIR" "$gURL" # download iso
-else 
-echo "Latest version .iso already exist in $gDIR"
+  then
+    wget -P "$gDIR" "$gURL" # download iso
+  else 
+    echo "Latest version .iso already exist in $gDIR"
 fi
 
 if ! [ -f "$gDIR/$gISO".CONTENTS.gz ]
-then
-wget -P "$gDIR" "$gURL.CONTENTS.gz" # download cont
-else
-echo "Latest version CONTENTS already exist in $gDIR"
+  then
+    wget -P "$gDIR" "$gURL.CONTENTS.gz" # download cont
+  else
+    echo "Latest version CONTENTS already exist in $gDIR"
 fi
+
+update-key
+
+sha-verifi
+
 }
 
 
@@ -61,20 +67,25 @@ gSHA=$(sha512sum "$gDIR"/"$gISO" | cut -d ' ' -f 1) #get SHA512
 fSHA=$(grep -E '^\w+.+iso$' "$gDIR"/"$gISO".DIGESTS | cut -d ' ' -f 1) #get SHA512 too
 
 if [ "$gSHA" = "$fSHA" ]
-then
-echo "SHA256 verification successful!"
-else
-echo "ERROR! SHA256 verification failed!"
+  then
+    echo "SHA256 verification successful!"
+  else
+    echo "ERROR! SHA256 verification failed!"
 fi
 }
 
+#remaster() {}
+
+
+
 #todo*************************remastering!!
-select number in 1 2 3 4
+echo "SH script for download latest Gentoo Linux and optionally add company ssh keys to .iso"
+select number in "Check, download and verifi new .iso" "Verifi existing .iso" "Remastering" "4-Exit"
 do  
   case $number in
-    1) update-key;;
-    2) resdownload;;
-    3) sha-verifi;;
+    1) resdownload;;
+    2) update-key && sha-verifi;;
+    3) remaster;;
     4) break;;
     *) echo something wrong;;
   esac
