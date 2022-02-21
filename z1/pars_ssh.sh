ac1a189af72726a70b0609a81387ef9890cc2040
Foggerman <foggermanishe@protonmail.com>
2022-02-21 12:20:46 +0300

    z1 Добавил проверку наличия iptables.rules и вывод информации.

--- z1/pars_ssh.sh
+++ z1/pars_ssh.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #need root
-#1. Написать bash скрипт который осуществляет поиск количества попыток #взлома по протоколу ssh с каждого ip и блокировку последнего с #помощью iptables, если количество попыток превысило 10. В данном #случае используйте регулярные выражения для поиска ip, отсортируйте #результат по количеству попыток взлома для вывода результата. Пример #файла журнала: sshd-test.log
-
+#1. Написать bash скрипт который осуществляет поиск количества попыток #взлома
+# по протоколу ssh с каждого ip и блокировку последнего с #помощью iptables, если количество попыток превысило 10. 
+#В данном #случае используйте регулярные выражения для поиска ip, отсортируйте #результат по количеству попыток взлома для вывода результата. Пример #файла журнала: sshd-test.log
 #********************************
-<<<<<<< HEAD
 logPATH=sshd-test.log
 #iptSET=/etc/network/iptables.rules
-bIP=$(grep -Eo "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}" "$logPATH" | sort | uniq -c) #фильтруем все ip, нужно предусмотреть защиту белых, кто просто часто логинится или забывает пароль) 
+bIP=$(grep -Eo "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}" "$logPATH" | sort | uniq -c) #фильтруем все ip, нужно предусмотреть защиту белых, кто просто часто логинится или забывает пароль) (Accepted password for root from 192.168.6.200 port 14543 ssh2)
 
 if [[ $EUID -ne 0 ]]; then
 	echo "This script must be run as root"
@@ -16,33 +16,25 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 IFS=$'\n' #change default delimiter
-
-sudo iptables-restore < iptables.rules #restore iptables settings
-=======
-bIP=$(grep -Eo "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}" sshd-test.log | sort | uniq -c)
-IFS=$'\n'
->>>>>>> master
+if [ -f iptables.rules ]; then
+iptables-restore < iptables.rules #restore iptables settings
+fi
 for ip in $bIP
 do 
     if [ "$(echo "$ip" | sed 's/^[ \t]*//' | cut -d ' ' -f 1)" -gt 10 ]
     then
     badIP=$(echo "$ip" | sed 's/^[ \t]*//' | cut -d ' ' -f 2)
+        echo "Block IP: $badIP"
         if ! sudo iptables -I INPUT -s "$badIP" -j DROP #to do block nftables
         then 
         echo "ERROR! $badIP do not DROPd"
         fi
     fi
 done
-<<<<<<< HEAD
 
-sudo iptables-save > iptables.rules #save iptables settings
+iptables-save > iptables.rules #save iptables settings
 IFS=$' ' #return  default delimiter
 
 #****************************
 #с готовым логом просто, но нужно придумать вариант работы в виде демона с фильтрацией в реальном времени.
 #tail -f "$logPATH" | grep -Eo "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}"
-=======
-IFS=$' '
-#****************************
-#с готовым логом просто, но нужно придумать вариант работы в виде демона с фильтром в реальном времени.
->>>>>>> master
